df5b9e6f5f2fd95a27b21b7cd7e2efb22ce2f579
fangwenjie <colawalnut@gmail.com>
2020-12-06 12:12:39 +0800

    init project

---
+++ UITableViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewSample
+//
+//  Created by 方文杰 on 2020/12/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
